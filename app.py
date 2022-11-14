5539e0f521fff78e4fa46174a46ddfe4067ab6e4
Kuzin Dmitriy <59863263+DmKuzin@users.noreply.github.com>
2022-11-14 23:46:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,11 @@
-import numpy as np
-import pandas as pd
-from flask import Flask, request, render_template
-from sklearn import preprocessing
-import pickle
-
+from flask import Flask
+ 
 app = Flask(__name__)
-#model = keras.models.load_model('densnet121_fish.h5')
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/predict',methods=['POST'])
-def predict():
-    # feature_list = request.form.to_dict()
-    # feature_list = list(feature_list.values())
-    # feature_list = list(map(int, feature_list))
-    # final_features = np.array(feature_list).reshape(1, 12)
-    #
-    # prediction = model.predict(final_features)
-    # output = int(prediction[0])
-    # if output == 1:
-    #     text = ">50K"
-    # else:
-    #     text = "<=50K"
-
-    text = 'test_app'
-    #return render_template('index.html', prediction_text='Employee Income is {}'.format(text))
-    return 'test'
-
-
+ 
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    return "Boston House Price Prediction”
+ 
+ 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+   app.run()
