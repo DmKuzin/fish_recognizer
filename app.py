7c6e24728e1d186e85e99191385fa79a00a78274
Kuzin Dmitriy <59863263+DmKuzin@users.noreply.github.com>
2022-11-15 19:02:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 #import numpy as np
 #import pandas as pd
 from flask import Flask, request, render_template
-#from PIL import Image
-import tensorflow as tf
+from PIL import Image
+#import tensorflow as tf
 #from sklearn import preprocessing
 #import pickle
 
@@ -18,16 +18,16 @@ def predict():
     imagefile = request.files.get('img', '')
     file = request.files['img']
     
-    #pil_image = Image.open(file)
+    pil_image = Image.open(file)
     
-    bits = tf.io.read_file(file)
+    #bits = tf.io.read_file(file)
 
     text = "<=50K"
     #text = str(list(feature_list.values())[0])
     print('imagefile:', imagefile)
     print('file:', file)
-    #print('pil_image:', pil_image)
-    print('bits:', bits)
+    print('pil_image:', pil_image)
+    #print('bits:', bits)
   
     return render_template('index.html', prediction_text='Employee Income is {}'.format(text))
 
