e6c83bdbb506e12dd46c3213478a2c2771ed5f62
Kuzin Dmitriy <59863263+DmKuzin@users.noreply.github.com>
2022-11-15 18:12:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
     #feature_list = request.form.to_dict()
-    imagefile = request.files.get('myfile', '')
+    imagefile = request.files.get('imagefile', '')
     
 
     text = "<=50K"
