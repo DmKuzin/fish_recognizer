333b929b6df0cd8ed49a23a7c1bb7bb0eb1babe9
Kuzin Dmitriy <59863263+DmKuzin@users.noreply.github.com>
2022-11-17 11:19:31 +0300

    add open file from flask

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 import numpy as np
 import pandas as pd
-from flask import Flask, request, render_template
+from flask import Flask, request, flash, redirect, render_template
 from PIL import Image, ImageOps
 import tensorflow as tf
 import pickle
+import werkzeug
 
 app = Flask(__name__)
 
 # input image size
 image_size = (256, 256)
+# allowed extensions
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
+
+
+# check file on allowed extension
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 # resize image function
@@ -22,6 +31,7 @@ image_size = (256, 256)
 #     padding = (pad_width, pad_height, delta_width - pad_width, delta_height - pad_height)
 #     return ImageOps.expand(img, padding)
 
+# Open file and convert to input network format
 def decode_image_from_file(filename, image_size=image_size):
     bits = tf.io.read_file(filename)
     image = tf.image.decode_jpeg(bits, channels=3)
@@ -56,14 +66,29 @@ df_label_name_load = pd.read_csv('models/label_name.csv', index_col=0)
 @app.route('/predict', methods=['POST'])
 def predict():
     # read file from request
+    #file = request.files['img']
+
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
     file = request.files['img']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = werkzeug.utils.secure_filename(file.filename)
+        print(filename)
+
     # open file as image
-    #pil_image = Image.open(file)
+    # pil_image = Image.open(file)
     arr_img_sqr = decode_image_from_file(file)
     # resize image to network input
-    #pil_img_sqr = resize_with_padding(pil_image, expected_size=image_size)
+    # pil_img_sqr = resize_with_padding(pil_image, expected_size=image_size)
     # normalize image and convert to tensor
-    #arr_img_sqr = np.asarray(pil_img_sqr) / 256.0
+    # arr_img_sqr = np.asarray(pil_img_sqr) / 256.0
     # make prediction
     # pred_idx = pd.DataFrame(model.predict(arr_img_sqr[None, :])).idxmax(axis=1)[0]
     # prediction = df_label_name_load.iloc[[pred_idx]]['rus'].values[0]
